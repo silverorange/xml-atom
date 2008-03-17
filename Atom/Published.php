be491879fcdef19b7a367817a2805a252a7c5fc6
Michael Gauthier <mike@silverorange.com>
2008-03-17 02:40:26 +0000

    PEARify documentation
    svn commit r28966

--- Atom/Published.php
+++ Atom/Published.php
@@ -2,26 +2,42 @@
 
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
+/**
+ * Date class definition.
+ */
 require_once 'XML/Atom/Date.php';
 
+// {{{ class XML_Atom_Published
+
 /**
- * A class used to used to generate a published date node.
+ * An {@link XML_Atom_Date} that indicates the date an entry or feed was
+ * published
  *
+ * Feeds and entries may have one or zero published dates. If an entry does not
+ * have a publiched date, the published date of the parent feed or source
+ * applies to the entry. In this case, the feed or source <em>must</em> have a
+ * published date.
+ *
+ * @category  XML
  * @package   XML_Atom
+ * @author    Michael Gauthier <mike@silverorange.com>
  * @copyright 2008 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @link      http://pear.php.net/package/XML_Atom
+ * @see       XML_Atom_Entry::setPublished()
+ * @see       XML_Atom_Feed::setPublished()
  */
 class XML_Atom_Published extends XML_Atom_Date
 {
     // {{{ protected function _createNode()
 
     /**
-     * Creates a published node
+     * Creates a published DOMElement node for this published date
      *
-     * @param DOMNode $context_node the parent node that will contain this
-     *   published node.
+     * @param DOMNode $context_node the context node in the XML document used
+     *                              to create this node.
      *
-     * @return DOMNode the new published node.
+     * @return DOMNode the author DOMElement node.
      */
     protected function _createNode(DOMNode $context_node)
     {
@@ -32,5 +48,7 @@ class XML_Atom_Published extends XML_Atom_Date
 
     // }}}
 }
+
+// }}}
 
 ?>
