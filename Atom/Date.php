7b181ea5d69f62ea093e09f407e77bc3fe1f570c
Michael Gauthier <mike@silverorange.com>
2008-03-17 03:56:15 +0000

    PEARify date class.
    svn commit r28967

--- Atom/Date.php
+++ Atom/Date.php
@@ -2,24 +2,40 @@
 
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
+/**
+ * PEAR Date class definitions.
+ */
 require_once 'Date.php';
-require_once 'XML/Atom/Element.php';
 
 /**
- * A class used to generate a date node.
+ * Element class definition.
+ */
+require_once 'XML/Atom/Element.php';
+
+// {{{ class XML_Atom_Date
+
+/**
+ * Abstract base class used to describe a date in an Atom document
  *
+ * This class is an Atom wrapper for a PEAR Date object.
+ *
+ * @category  XML
  * @package   XML_Atom
+ * @author    Michael Gauthier <mike@silverorange.com>
  * @copyright 2008 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @link      http://pear.php.net/package/XML_Atom
  */
 abstract class XML_Atom_Date extends XML_Atom_Element
 {
     // {{{ protected properties
 
     /**
-     * The date of this node
+     * The date represented by this element
      *
      * @var Date
+     *
+     * @see XML_Atom_Date::setDate()
      */
     protected $_date;
 
@@ -27,9 +43,12 @@ abstract class XML_Atom_Date extends XML_Atom_Element
     // {{{ public function __construct()
 
     /**
-     * Contructs this XML_Atom_Person
+     * Creates a new Date
      *
-     * @param string $date the date to use.
+     * @param string|Date $date the date to use. This can be either an already
+     *                          constructed Date object or a textual
+     *                          representation of a Date that can be parsed by
+     *                          the Date class.
      */
     public function __construct($date)
     {
@@ -40,9 +59,14 @@ abstract class XML_Atom_Date extends XML_Atom_Element
     // {{{ public function setDate()
 
     /**
-     * Sets the date of this node
+     * Sets the date of this date element
      *
-     * @param mixed $date the date this node should use.
+     * @param string|Date $date the date to use. This can be either an already
+     *                          constructed Date object or a textual
+     *                          representation of a Date that can be parsed by
+     *                          the Date class.
+     *
+     * @return void
      */
     public function setDate($date)
     {
@@ -62,6 +86,16 @@ abstract class XML_Atom_Date extends XML_Atom_Element
      * @param DOMNode $node the date node that will contain all the XML created
      *   by this node.
      */
+    /**
+     * Builds and creates the Atom XML nodes required by this date
+     *
+     * The element node representing this date is created separately and
+     * passed as the first parameter of this method.
+     *
+     * @param DOMNode $node the node representing this date.
+     *
+     * @return void
+     */
     protected function _buildNode(DOMNode $node)
     {
         $document = $node->ownerDocument;
@@ -74,5 +108,7 @@ abstract class XML_Atom_Date extends XML_Atom_Element
 
     // }}}
 }
+
+// }}}
 
 ?>
