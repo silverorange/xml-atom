974025dc946d4f2a4a2647dd3b5587fae9d73faf
Keith Burgoyne <keith@silverorange.com>
2010-10-20 14:56:25 +0000

    prepare for release of 0.1.7
    svn commit r58160

--- package.php
+++ package.php
@@ -5,7 +5,7 @@
 require_once 'PEAR/PackageFileManager2.php';
 PEAR::setErrorHandling(PEAR_ERROR_DIE);
 
-$release_version = '0.1.6';
+$release_version = '0.1.7';
 $release_state   = 'alpha';
 $release_notes   =
     'First release.';
