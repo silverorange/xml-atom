276bcc40e98c239a5ff02400e7cb642b6e851c18
Keith Burgoyne <keith@silverorange.com>
2013-03-08 19:43:55 +0000

    prepare for release of 0.1.8
    svn commit r79279

--- package.php
+++ package.php
@@ -5,7 +5,7 @@
 require_once 'PEAR/PackageFileManager2.php';
 PEAR::setErrorHandling(PEAR_ERROR_DIE);
 
-$release_version = '0.1.7';
+$release_version = '0.1.8';
 $release_state   = 'alpha';
 $release_notes   =
     'First release.';
