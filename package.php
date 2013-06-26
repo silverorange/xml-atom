0652eb24c620f90e8c28ce13145f5424e3ecafd5
Keith Burgoyne <keith@silverorange.com>
2013-06-26 20:01:40 +0000

    prepare for release of 1.0.0 | package party
    svn commit r81926

--- package.php
+++ package.php
@@ -5,7 +5,7 @@
 require_once 'PEAR/PackageFileManager2.php';
 PEAR::setErrorHandling(PEAR_ERROR_DIE);
 
-$release_version = '0.1.8';
+$release_version = '1.0.0';
 $release_state   = 'alpha';
 $release_notes   =
     'First release.';
